61299a4d1f5081137d07fc9c21b114bc4cafaac9
Tim Macdonald <tsmacdonald@gmail.com>
2013-12-03 14:16:19 -0600

    Formatting issues; add P and C POSes

--- pos.py
+++ pos.py
@@ -22,7 +22,7 @@ freq_dist = FreqDist(all_text_lower)
 
 # make a reduced vocabulary (here, 500 types)
 vocab = freq_dist.keys()[:500]
-vocab = vocab + ["***"]
+vocab.append('***')
 
 
 
@@ -30,31 +30,37 @@ vocab = vocab + ["***"]
 # 2. Make a reduced form of the PennTB tagset
 penntb_to_reduced = {}
 # noun-like
-for x in ['NN', 'NNS', 'NNP', 'NNPS', 'PRP', 'EX', 'WP'] :
+for x in ['NN', 'NNS', 'NNP', 'NNPS', 'PRP', 'EX', 'WP']:
     penntb_to_reduced[x] = 'N'
 # verb-like
-for x in ['VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ', 'MD', 'TO'] :
+for x in ['VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ', 'MD', 'TO']:
     penntb_to_reduced[x] = 'V'
 # adjective-like
 for x in ['POS', 'PRP$', 'WP$', 'JJ', 'JJR', 'JJS', 'DT', 'CD', 'PDT', 'WDT', 'LS']:
     penntb_to_reduced[x] = 'AJ'
 # adverb-like
-for x in ['RB', 'RBR', 'RBS', 'WRB', 'RP', 'IN', 'CC']:
+for x in ['RB', 'RBR', 'RBS', 'WRB']:
     penntb_to_reduced[x] = 'AV'
+# preposition-like
+for x in ['IN', 'RP']:
+	penntb_to_reduced[x] = 'P'
+# conjunctions
+for x in ['CC']:
+	penntb_to_reduced[x] = 'C'
 # interjections
-for x in ['FW', 'UH'] :
+for x in ['FW', 'UH']:
     penntb_to_reduced[x] = 'I'
 # symbols
-for x in ['SYM', '$', '#'] :
+for x in ['SYM', '$', '#']:
     penntb_to_reduced[x] = 'S'
 # groupings
-for x in ['\'\'', '(', ')', ',', ':', '``', '"'] :
+for x in ['\'\'', '(', ')', ',', ':', '``', '"']:
     penntb_to_reduced[x] = 'G'
 # end-of-sentence symbols
-for x in ['.', '!', '?'] :
+for x in ['.', '!', '?']:
     penntb_to_reduced[x] = 'E'
 
-reduced_tags = ['N', 'V', 'AJ', 'AV', 'I', 'S', 'G', 'E']
+reduced_tags = ['N', 'V', 'AJ', 'AV', 'I', 'S', 'G', 'E', 'P', 'C']
 
 # 3. tag the corpus
 all_tagged = nltk.pos_tag(all_text)
@@ -95,42 +101,38 @@ for (word, tag) in all_tagged :
     if(word in vocab):  
         word_tag_tally[word][tag] += 1
 
-    tag_trans_tally[tag][previous_tag] += 1
+    tag_trans_tally[previous_tag][tag] += 1
     totals[tag] += 1
 
     previous_tag = tag
 
-
+# fill this out:
+#	For each tag tg1 compute the probabilities for transitioning to
+#	each tag (say, tg2). Using relative frequency estimation,
+#	that would mean dividing the number of times tg2 follows tg1 by
+#	the absolute number of times t1 occurs. (But, what if tg1 never occurs..?)
+#	Recommendation: think in terms of "for each tg2, how many times had
+#	we transitioned from tg1?"
 
 # now, make the actual transition probability matrices 
 trans_probs = {y:{x:0 for x in reduced_tags} for y in reduced_tags}
-for tg1 in reduced_tags :
+for tg1 in reduced_tags:
     for tg2 in reduced_tags:
         trans_probs[tg1][tg2] = tag_trans_tally[tg1][tg2] / totals[tg1]
 
-print trans_probs        
+print trans_probs 
 
-    # fill this out:
-        # For each tag tg1 compute the probabilities for transitioning to
-        # each tag (say, tg2). Using relative frequency estimation,
-        # that would mean dividing the number of times tg2 follows tg1 by
-        # the absolute number of times t1 occurs. (But, what if tg1 never occurs..?)
-        # Recommendation: think in terms of "for each tg2, how many times had
-        # we transitioned from tg1?"
-
-
+# Fill this out:
+#	For each tag tg1 compute the probabilities for emitting each word v.
+#	Recommendation: think in terms of "for each word v, how many times
+#	did tg1 emit v?"
 emit_probs = {y:{x:0 for x in vocab} for y in reduced_tags}
-for tg in reduced_tags :
+for tg in reduced_tags:
     for wd in vocab:
         emit_probs[tg][wd] = word_tag_tally[wd][tg] / totals[tg]
 
 print emit_probs
 
-    # fill this out:
-        # For each tag tg1 compute the probabilities for emitting each word v.
-        # Recommendation: think in terms of "for each word v, how many times
-        # did tg1 emit v?"
-    
 
 # 5. implement Viterbi. 
 # Write a function that takes a sequence of tokens,
@@ -139,7 +141,7 @@ print emit_probs
 
 def pos_tagging(sequence, trans_probs, emit_probs, vocab, tags, start) :
 
-    # fill this out
+    #TODO
 
     tagged = []
 
