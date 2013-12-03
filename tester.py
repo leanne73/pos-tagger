1c8202bce4a20c82a83225facff9b12b175cf4c5
Leanne Miller <leanne.miller@my.wheaton.edu>
2013-12-03 17:43:55 -0600

    Update tester

--- tester.py
+++ tester.py
@@ -1,15 +1,37 @@
+from __future__ import division
+import nltk
+from nltk.corpus import PlaintextCorpusReader
+import pos
+
 correct_tags = []
 with open('baum-test-hand.txt') as hand:
 	for line in hand:
-		token, tag = line.split()
-		correct_tags.append((token, tag))
+		token, tag = line.strip().split()
+		correct_tags.append(tag)
 
-generated_tags = [] 
-with open('baum-test-machine.txt') as machine:
-	for line in machine:
-		token, tag = line.split()
-		generated_tags.append((token, tag))
+trans_probs, emit_probs, tags, start = pos.main()
+with open('baum-tiny-test.txt') as corpus:
+	words = " ".join(corpus)
+	generated_tags = pos.pos_tagging(words, trans_probs, emit_probs, tags, start)
 
 #crank NLTK magic here
+reader = PlaintextCorpusReader('.', '.*\.txt')
+test_text = nltk.Text(reader.words('baum-tiny-test.txt'))
+
+nltk_tags = nltk.pos_tag(test_text)
+nltk_tags = [pos.nltk_to_normalized_tag(tag) for (word, tag) in nltk_tags]
+
 
 #crank magic comparing correct_ and generated_tags and the NLTK tags
+
+def percent_correct(tags):
+	correct = 0
+	for x in range(len(tags)):
+		if tags[x] == correct_tags[x]:
+			correct += 1
+	return (correct / len(tags)) * 100
+
+print("\nPercent Correct of Our Tags:")
+print percent_correct(generated_tags)
+print("\nPercent Correct of nltk Tags:")
+print percent_correct(nltk_tags)
